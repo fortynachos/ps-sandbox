090bf68f6b1da10aec0eff8f29cf8c09cfd7af55
Tayyab Hussain <tayyabmh93@gmail.com>
2018-03-22 15:25:19 -0400

    did the dashboard list view

--- pendo-experience/src/dashboard/dashboard-list-view.js
+++ pendo-experience/src/dashboard/dashboard-list-view.js
@@ -4,10 +4,41 @@ import './dashboard.css';
 
 
 const data = [
-
+	{
+		"test1": "Example",
+		"test2": "Commit"
+	},
+	{
+		"test1": "Example",
+		"test2": "Commit"
+	},
+	{
+		"test1": "Example",
+		"test2": "Commit"
+	},
+	{
+		"test1": "Example",
+		"test2": "Commit"
+	},
+	{
+		"test1": "Example",
+		"test2": "Commit"
+	},
+	{
+		"test1": "Example",
+		"test2": "Commit"
+	},
+	{
+		"test1": "Example",
+		"test2": "Commit"
+	},
+	{
+		"test1": "Example",
+		"test2": "Commit"
+	}
 ];
 
-
+var i = 0;
 
 
 const ListView = () => (
@@ -16,8 +47,34 @@ const ListView = () => (
 			Open Opportunities
 		</h2>
 		<div className="list-container">
-			<div className="list-right">
-				<p> stage</p>
+			<div className="header-container">
+				<div className="list-left">
+					<p>Deal</p>
+				</div>
+				<div className="list-right">
+					<p>Stage</p>
+				</div>
+			</div>
+			<div className="list-table">
+				{
+					data.map(function(content) {
+						if (i%2 === 0) {
+							i++;
+							return (
+									<div className="list-item blue" key={i}>
+										<span>{content.test1}</span> <span>{content.test2}</span>
+									</div>
+							);
+						} else {
+							i++;
+							return(
+								<div className="list-item white" key={i}>
+									<span>{content.test1}</span> <span>{content.test2}</span>
+								</div>
+							);
+						}
+					})
+				}
 			</div>
 		</div>
 	</div>
