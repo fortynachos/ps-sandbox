fb2128311834037dbfd423f492ce1fc6dfefccf6
MoaadBenkaraache <moaadbenkaraache@MoaadBenkaraache.local>
2018-04-09 15:26:12 -0400

    reducer comma

--- pendo-experience/src/reducers/index.js
+++ pendo-experience/src/reducers/index.js
@@ -6,7 +6,7 @@ import { combineReducers } from 'redux';
 const rootReducer = combineReducers({
 	Navigation,
 	TimelineReducer,
-	accountReducer
+	accountReducer,
 });
 
 export default rootReducer;
