666e55db804d485a98f436343b202510468da392
MoaadBenkaraache <moaadbenkaraache@MoaadBenkaraache.local>
2018-04-09 16:41:24 -0400

    merge attempt 3

--- pendo-experience/src/actions/index.js
+++ pendo-experience/src/actions/index.js
@@ -10,30 +10,6 @@ export const updateDetailsInputValue = (text) => ({
 	text
 });
 
-
-
-export const requestAccounts = () => {
-	return {
-		type: 'REQUEST_ACCOUNTS',
-		loading: true
-	}
-}
-
-export const receiveAccounts = () => {
-	return {
-		type: 'RECEIVED_ACCOUNTS',
-		loading: false,
-		accounts:
-		  {
-		    key: '1',
-		    name: 'John Brown',
-		    rep: 32,
-		    territory: 'New York No. 1 Lake Park',
-		  }
-
-	}
-}
-
 var icons = {
 	'New Note': 'form',
 	'Email': 'mail',
@@ -41,10 +17,10 @@ var icons = {
 	'Log Activity': 'tool',
 	'Create Task': 'check-circle-o',
 	'Schedule': 'calendar'
-};
+}
 
 export const addPostToTimeline = (icon, entry) => ({
 		type: 'ADD_POST_TO_TIMELINE',
 		icon: icons[icon],
 		entry
-	});
+});
--- pendo-experience/src/reducers/index.js
+++ pendo-experience/src/reducers/index.js
@@ -1,13 +1,11 @@
 import Navigation from './navigation';
 import TimelineReducer from './timeline';
-import accountReducer from './accounts';
 import { combineReducers } from 'redux';
 import PipelineReducer from './pipeline';
 
 const rootReducer = combineReducers({
 	Navigation,
 	TimelineReducer,
-	accountReducer,
 	PipelineReducer
 });
 
