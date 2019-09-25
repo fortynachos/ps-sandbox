efa25a97782359b343bcbc670b92c303f17a9c66
Mike Fotinatos <michael.fotinatos@gmail.com>
2019-09-25 10:55:14 -0400

    sfdc

--- src/components/body/body.js
+++ src/components/body/body.js
@@ -154,6 +154,7 @@ export default class Body extends React.Component {
 						<h1>
 							{this.props.title}
 						</h1>
+						<a id="liveagent_button_online_5734P000000kWNg" href="javascript://Chat" onclick="liveagent.startChat('5734P000000kWNg')">Hey Listen!</a><div id="liveagent_button_offline_5734P000000kWNg">Reach out now!</div>
 					</div>
 					<div className="main-content">
 						<Main />
